3f482cf374b06c546770d31004b2b1beb5f97e4c
Chad Woolley <thewoolleyman@gmail.com>
2016-04-20 16:49:04 -0700

    Hack in RubyMine extension paths to load path so it doesn't fail to require dependencies if Spring server was started outside of RubyMine.

--- lib/spring/commands/rspec.rb
+++ lib/spring/commands/rspec.rb
@@ -15,6 +15,10 @@ module Spring
 
       def call
         ::RSpec.configuration.start_time = Time.now if defined?(::RSpec.configuration.start_time)
+        # Hack in RubyMine extension paths to load path so it doesn't fail to require dependencies
+        # if Spring server was started outside of RubyMine.
+        $LOAD_PATH << '/Applications/RubyMine.app/Contents/rb/testing/patch/bdd/'
+        $LOAD_PATH << '/Applications/RubyMine.app/Contents/rb/testing/patch/common/'
         load Gem.bin_path(gem_name, exec_name)
       end
     end
